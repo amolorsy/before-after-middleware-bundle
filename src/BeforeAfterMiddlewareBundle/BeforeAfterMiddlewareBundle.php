ef3d297dd7b6c3d2f977ebc880adbd59073e75dc
Gyu Kang <gyu@gyukang.me>
2018-09-09 06:07:50 +0900

    Create a project

---
+++ src/BeforeAfterMiddlewareBundle/BeforeAfterMiddlewareBundle.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace BeforeAfterMiddlewareBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * @author Gyu Kang <gyu@gyukang.me>
+ */
+class BeforeAfterMiddlewareBundle extends Bundle
+{
+}
